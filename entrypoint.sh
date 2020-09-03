3e567a021f6113ecd3adc36588a02e7f72abd174
ayan-b <ayanbanerjee7777@gmail.com>
2020-09-03 12:50:19 +0530

    Only moban

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-moban
 ls
-docker run --mount src=$GITHUB_WORKSPACE/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 main.tex; biber main; tectonic main.tex"
-mv src/main.pdf src/$INPUT_OUTPUT_PDF
+moban
