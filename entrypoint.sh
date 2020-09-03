bae45f9c70cb1527923d77ae4dc18994d11039a2
ayan-b <ayanbanerjee7777@gmail.com>
2020-09-03 12:36:30 +0530

    Input output_pdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 moban
+docker run --mount src=$GITHUB_WORKSPACE/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 main.tex; biber main; tectonic main.tex"
+mv src/main.pdf src/$INPUT_OUTPUT_PDF
